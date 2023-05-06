ced8fc85e611b05ec4dd17739c0ffc11ffb41e66
Andrea González <andrea.gonzalez14131@est.uca.edu.ni>
2023-05-06 17:35:12 -0600

    Menú desplegable

--- app/src/main/java/com/example/appfavas/MainActivity.kt
+++ app/src/main/java/com/example/appfavas/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.appfavas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.appfavas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val iniciar = Intent(this, LayoutDrawableActivity::class.java)
+            startActivity(iniciar)
+        }
+
     }
 }
