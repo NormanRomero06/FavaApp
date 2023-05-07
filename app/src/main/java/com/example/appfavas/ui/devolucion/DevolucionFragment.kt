5a8055f8539a45b76aaf9ae507f8bb149049f2a2
Andrea González <andrea.gonzalez14131@est.uca.edu.ni>
2023-05-06 21:54:10 -0600

    Pantalla de Devolucion

--- app/src/main/java/com/example/appfavas/ui/devolucion/DevolucionFragment.kt
+++ app/src/main/java/com/example/appfavas/ui/devolucion/DevolucionFragment.kt
@@ -1,18 +1,18 @@
-package com.example.appfavas.ui.frutas
+package com.example.appfavas.ui.devolucion
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.appfavas.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-class CrudFrutaFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DevolucionFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DevolucionFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -20,8 +20,6 @@ class CrudFrutaFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -30,7 +28,7 @@ class CrudFrutaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crud_fruta, container, false)
+        return inflater.inflate(R.layout.fragment_devolucion, container, false)
     }
 
     companion object {
@@ -40,15 +38,13 @@ class CrudFrutaFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CrudFrutaFragment.
+         * @return A new instance of fragment DevolucionFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CrudFrutaFragment().apply {
+            DevolucionFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
