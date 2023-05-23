c156f0f691a90cb736740519423260a74b251338
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-05-22 23:14:47 -0600

    Vista de los productos

--- app/src/main/java/com/example/appfavas/ui/articulo/TotalArticulosVentasFragment.kt
+++ app/src/main/java/com/example/appfavas/ui/articulo/TotalArticulosVentasFragment.kt
@@ -6,29 +6,76 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.appfavas.R
 import com.example.appfavas.databinding.FragmentTotalArticulosVentasBinding
+import com.example.appfavas.decoration.SpaceItemDecoration
+import com.example.appfavas.modelos.Articulo.Articulo
+import com.example.appfavas.modelos.Articulo.ArticuloAdapter
 
 class TotalArticulosVentasFragment : Fragment() {
-    private var _binding: FragmentTotalArticulosVentasBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentTotalArticulosVentasBinding
+    val artList = arrayListOf<Articulo>()
+    var recyclerView: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentTotalArticulosVentasBinding.inflate(inflater, container, false)
+        binding = FragmentTotalArticulosVentasBinding.inflate(inflater, container, false)
 
         val root: View = binding.root
         navigation()
+        cargarProductos()
 
         return root
     }
 
+     fun cargarProductos(){
+
+            val uri ="http://localfavas.online/Producto/ReadProducto.php"
+
+            recyclerView = binding.rvArticulos
+            val reqQueue: RequestQueue = Volley.newRequestQueue(getActivity())
+            val request = JsonObjectRequest(Request.Method.GET, uri, null, { res ->
+                val jsonArray = res.getJSONArray("data")
+
+                for (i in 0 until jsonArray.length()){
+                    val jsonObj = jsonArray.getJSONObject(i)
+                    val user = Articulo(
+                        jsonObj.getInt("idProducto"),
+                        jsonObj.getString("nombre"),
+                        jsonObj.getDouble("precio").toFloat(),
+                        jsonObj.getInt("cantidad")
+                    )
+                    artList.add(user)
+                }
+                println(artList.toString())
+
+                val spanCount = 2 // Número de columnas en la cuadrícula
+                val spaceHorizontal = resources.getDimensionPixelSize(R.dimen.item_space_horizontal)
+                val spaceVertical = resources.getDimensionPixelSize(R.dimen.item_space_vertical)
+                val gridLayoutManager = GridLayoutManager(requireContext(), spanCount)
+                val space = resources.getDimensionPixelSize(R.dimen.item_space) // Define el tamaño del espacio
+                recyclerView?.addItemDecoration(SpaceItemDecoration(spaceHorizontal, spaceVertical))
+                recyclerView?.layoutManager = gridLayoutManager
+                recyclerView?.adapter = ArticuloAdapter(artList)
+
+
+
+            },{
+
+            })
+
+            reqQueue.add(request)
+        }
     fun navigation()
     {
         binding.btnNuevoArticulo.setOnClickListener {
@@ -43,6 +90,6 @@ class TotalArticulosVentasFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding
     }
 }
