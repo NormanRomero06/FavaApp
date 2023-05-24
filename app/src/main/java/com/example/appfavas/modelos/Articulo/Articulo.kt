07719a1d711ad47ef1b2593f5abb325d6dc052b0
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-05-24 00:53:52 -0600

    Data class y adapter para CRUD Articulo

--- app/src/main/java/com/example/appfavas/modelos/Articulo/Articulo.kt
+++ app/src/main/java/com/example/appfavas/modelos/Articulo/Articulo.kt
@@ -5,10 +5,10 @@ import android.graphics.Bitmap
 data class Articulo(
     var idProducto: Int,
     var nombre: String,
-    //var descripcion: String,
+    var descripcion: String,
     var precio: Float,
     var cantidad: Int,
     //var cantidaMinima: Int,
     //var imagen: Bitmap,
-    //var Categoria_idCategoria: Int
+    //var Categoria_Nombre: String
 )
--- app/src/main/java/com/example/appfavas/modelos/Articulo/ArticuloAdapter.kt
+++ app/src/main/java/com/example/appfavas/modelos/Articulo/ArticuloAdapter.kt
@@ -1,31 +1,47 @@
 package com.example.appfavas.modelos.Articulo
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appfavas.R
-import com.example.appfavas.databinding.ItemCategoriaBinding
-import com.example.appfavas.databinding.ItemProductoBinding
 import com.example.appfavas.databinding.ItemTotalArticulosVentasBinding
 
-class ArticuloAdapter(private val artList: ArrayList<Articulo>): RecyclerView.Adapter<ArticuloAdapter.ViewHolder>() {
+class ArticuloAdapter(private val artList: ArrayList<Articulo>) :
+    RecyclerView.Adapter<ArticuloAdapter.ViewHolder>() {
 
-    class ViewHolder(private val binding: ItemTotalArticulosVentasBinding): RecyclerView.ViewHolder(binding.root){
-        fun load(item: Articulo){
-            with(binding){
+    class ViewHolder(private val binding: ItemTotalArticulosVentasBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun load(item: Articulo) {
+            with(binding) {
+                tvIdProducto.text = item.idProducto.toString()
                 tvNombreArticulo.text = item.nombre
-                tvExistencia.text = item.cantidad.toString()
+                tvDescripcion.text = item.descripcion
                 tvPrecioArticulo.text = item.precio.toString()
-                cvArticulos.setOnClickListener {
-                    Navigation.findNavController(binding.root).navigate(R.id.editar_EliminarArticulosVentasFragment)
+                tvExistencia.text = item.cantidad.toString()
+                //tvCategoria.text = item.Categoria_Nombre
+                cwArticulo.setOnClickListener {
+                    val bundle = Bundle()
+                    bundle.putString("idProducto", item.idProducto.toString())
+                    bundle.putString("nombre", item.nombre)
+                    bundle.putString("descripcion", item.descripcion)
+                    bundle.putString("precio", item.precio.toString())
+                    bundle.putString("cantidad", item.cantidad.toString())
+                   //bundle.putString("Categoria_Nombre", item.Categoria_Nombre)
+                    Navigation.findNavController(binding.root)
+                        .navigate(R.id.editar_EliminarArticulosVentasFragment, bundle)
                 }
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val artiItem = ItemTotalArticulosVentasBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val artiItem = ItemTotalArticulosVentasBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return ViewHolder(artiItem)
     }
 
