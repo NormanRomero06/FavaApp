517a0e18d12a3c01763428a5334509b98bc963e7
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-05-24 00:51:37 -0600

    Data class y adapter para Historial de Articulos

---
+++ app/src/main/java/com/example/appfavas/modelos/Articulo/ArticuloVista.kt
@@ -0,0 +1,14 @@
+package com.example.appfavas.modelos.Articulo
+
+import android.graphics.Bitmap
+
+data class ArticuloVista(
+    //var idProducto: Int,
+    var nombre: String,
+    //var descripcion: String,
+    var precio: Float,
+    var cantidad: Int,
+    //var cantidaMinima: Int,
+    //var imagen: Bitmap,
+    //var Categoria_idCategoria: Int
+)
