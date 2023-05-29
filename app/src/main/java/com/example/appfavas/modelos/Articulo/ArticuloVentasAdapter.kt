4b798a14b94be7c32c65db9b3541cc0eda668139
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-05-28 23:09:16 -0600

    Se actualizó el adapter para mejoras del rcv

--- app/src/main/java/com/example/appfavas/modelos/Articulo/ArticuloVentasAdapter.kt
+++ app/src/main/java/com/example/appfavas/modelos/Articulo/ArticuloVentasAdapter.kt
@@ -9,44 +9,63 @@ import com.example.appfavas.R
 import com.example.appfavas.databinding.ItemProductoBinding
 
 
-class ArticuloVentasAdapter(private val ListArticulosVenta: ArrayList<ArticuloVentas>): RecyclerView.Adapter<ArticuloVentasAdapter.ViewHolder>() {
-    class ViewHolder(private val binding: ItemProductoBinding): RecyclerView.ViewHolder(binding.root){
-        fun load(item: ArticuloVentas) {
-            with(binding) {
-                var cantidad = 0
-                tvNombreProducto.text = item.nombre
-                PrecioTotal.text = item.precio.toString()
-                tvCantProd.text = cantidad.toString()
-            }
-            var cantidadTotal = 0
+class ArticuloVentasAdapter(private val ListArticulosVenta: ArrayList<ArticuloVentas>) :
+    RecyclerView.Adapter<ArticuloVentasAdapter.ViewHolder>() {
+
+    class ViewHolder(private val binding: ItemProductoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private var cantidadTotal = 0
+
+        init {
             binding.IvMas.setOnClickListener {
-                cantidadTotal++
-                binding.tvCantProd.text = cantidadTotal.toString()
+                incrementarCantidad()
             }
 
             binding.IvMenos.setOnClickListener {
-                if (cantidadTotal > 0) {
-                    cantidadTotal--
-                    binding.tvCantProd.text = cantidadTotal.toString()
-                }
+                decrementarCantidad()
             }
+        }
 
+        private fun incrementarCantidad() {
+            cantidadTotal++
+            binding.tvCantProd.text = cantidadTotal.toString()
+        }
+
+        private fun decrementarCantidad() {
+            if (cantidadTotal > 0) {
+                cantidadTotal--
+                binding.tvCantProd.text = cantidadTotal.toString()
+            }
+        }
+
+        fun load(item: ArticuloVentas) {
+            with(binding) {
+                tvNombreProducto.text = item.nombre
+                PrecioTotal.text = item.precio.toString()
+                tvCantProd.text = cantidadTotal.toString()
+            }
 
             binding.cwPrducto.setOnClickListener {
                 val bundle = Bundle()
-                bundle.putString("idProducto",item.idProducto.toString())
-                bundle.putString("nombre",item.nombre)
-                bundle.putString("precio",item.precio.toString())
+                bundle.putString("idProducto", item.idProducto.toString())
+                bundle.putString("nombre", item.nombre)
+                bundle.putString("precio", item.precio.toString())
+                bundle.putInt("cantidad", cantidadTotal)
+
+                val precioTotal = item.precio * cantidadTotal
+                bundle.putString("precioTotal", precioTotal.toString())
+
                 val navController = Navigation.findNavController(binding.root)
-                navController.navigate(R.id.cobroFragment,bundle)
+                navController.navigate(R.id.checkProductoFragment, bundle)
             }
         }
-        }
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val userItem = ItemProductoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val userItem =
+            ItemProductoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(userItem)
-
     }
 
     override fun getItemCount(): Int {
@@ -57,5 +76,7 @@ class ArticuloVentasAdapter(private val ListArticulosVenta: ArrayList<ArticuloVe
         holder.load(this.ListArticulosVenta[position])
     }
 }
+
+
 
 
