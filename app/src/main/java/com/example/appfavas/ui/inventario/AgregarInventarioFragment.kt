9a3912d80cb450b3bc9cb93aef2f990c9d7fca56
alecg01 <alec.godoy13083@est.uca.edu.ni>
2023-05-28 19:03:58 -0600

    Insert de Inventario con validacion

--- app/src/main/java/com/example/appfavas/ui/inventario/AgregarInventarioFragment.kt
+++ app/src/main/java/com/example/appfavas/ui/inventario/AgregarInventarioFragment.kt
@@ -1,60 +1,146 @@
 package com.example.appfavas.ui.inventario
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.appfavas.R
+import com.example.appfavas.databinding.FragmentAgregarInventarioBinding
+import com.example.appfavas.decoration.SpaceItemDecoration
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AgregarInventarioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AgregarInventarioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentAgregarInventarioBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar_inventario, container, false)
+    ): View {
+        binding = FragmentAgregarInventarioBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        cargarSpinner()
+        seleccionarMovimiento()
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AgregarInventarioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AgregarInventarioFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    fun seleccionarMovimiento() {
+        binding.spEntSal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedData: String = parent.getItemAtPosition(position).toString()
+
+                // Verificar el dato seleccionado y llamar al método correspondiente
+                if (selectedData == "Entrada") {
+                    entradaInventario()
+                } else if (selectedData == "Salida") {
+                    //salidaInventario()
                 }
             }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                binding.btnActualizar.setOnClickListener {
+                    Toast.makeText(context, "Seleccione un movimiento", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
+
+    fun entradaInventario() {
+        val idProducto = arguments?.getString("idProducto")
+        val nombre = arguments?.getString("nombre")
+        binding.tvIdProducto.setText(idProducto)
+        binding.tvNombreArticulo.text = nombre
+        binding.tvIdProducto.setText(idProducto)
+
+
+
+        binding.btnActualizar.setOnClickListener {
+            if (validarCampos()) {
+                try {
+                    val id = binding.tvIdProducto.text.toString()
+                    val movimiento = binding.spEntSal.selectedItem
+                    val cantidad = binding.etCantidad.text.toString()
+                    val url = "http://localfavas.online/Inventario/ActualizarEntradas.php"
+                    val queue = Volley.newRequestQueue(activity)
+                    val resultadoPost = object : StringRequest(
+                        Request.Method.POST, url,
+                        Response.Listener<String> { response ->
+                            Toast.makeText(
+                                context,
+                                "Insertado exitosamente",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }, Response.ErrorListener { error ->
+                            Toast.makeText(context, "Error: $error", Toast.LENGTH_LONG).show()
+                        }) {
+                        override fun getParams(): MutableMap<String, String> {
+                            val parametros = HashMap<String, String>()
+                            parametros.put("Producto_idProducto", id)
+                            parametros.put("tipoMovimiento", movimiento.toString())
+                            parametros.put("cantidad", cantidad)
+                            Log.d("Prueba1", "Parametros:$parametros")//Prueba
+                            return parametros
+
+                        }
+                    }
+                    queue.add(resultadoPost)
+                } catch (ex: Exception) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error al insertar: ${ex.toString()}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+
+    fun cargarSpinner() {
+        val spinner: Spinner = binding.spEntSal
+        val datos: List<String> = listOf("Entrada", "Salida")
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, datos)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun validarCampos(): Boolean {
+        var valido = true
+        val cantidad = binding.etCantidad.text.toString()
+
+        if (cantidad.isNullOrEmpty()) {
+            binding.etCantidad.setError("Por favor indique la cantidad")
+            valido = false
+        }
+        return valido
+    }
+
+
 }
